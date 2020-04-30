e0de8a8017db789ec4c7617e9b29b4acc700a46f
wedusganteng <63887769+wedusganteng@users.noreply.github.com>
2020-04-30 08:51:52 +0700

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,74 @@
+#!/system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
+#Pm Gua Bangsat
+clear
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 0.5
+
+clear
+figlet MR. W3du5 |lolcat
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : Mr.W3du5"
+echo "\033[37;1mFeat: Error404"
+echo "\033[35;1mversion Tools: 1"
+echo "\033[35;1mTeam : INDONESIA ERROR TEAM"
+echo "\033[33;1mKontak Me :  0895355578413"
+echo "\033[92mIndonesian Error Team" 
+echo "\033[36;1mIndonesia Security Lite"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+
+echo "[===============================================[>"
+echo $green "1.> Dark fb v1"
+echo "[===============================================]>"
+echo $blue "2.> Dark fb v2"
+echo "[===============================================[>"
+echo $yellow "0.> keluar"
+echo "[+]===============================================[+]"
+echo "\033[32;1m"
+read -p "root@T00Ls-Mr.W3du5~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet MR. W3du5 |lolcat
+git clone https://github.com/wedus56/MR.Wedus
+cd MR.Wedus
+python2 dkfb
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet MR. W3du5 |lolcat
+git clone https://github.com/wedusganteng/Darkfb
+cd Darkfb
+pip2 install -r password.txt
+python2 dark.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 0]
+then 
+echo "\033[321mTerima Kasih sudh pakai Tools saya ^_^"
+sleep 0.5
+echo "\033[33;1mJangan Lupa Bernafas sayang:v"
+sleep 0.5
+echo "\033[31;1mKalau Ada error silahkan hubungi saya di WA"
+sleep 0.5 
+echo "Whatsapp : +62895355578413"
+sleep 1
+exit
+fi
